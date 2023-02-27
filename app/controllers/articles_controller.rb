287296bacc33d0ae2584640502131355fb6c193e
Punyashlok Dash <punyashlok.com.@gmail.com>
2023-02-26 20:38:50 -0800

    Handling Edit and Update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,11 +7,31 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
-  def new; end
+  def new
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
 
   def create
     @article = Article.new(params.require(:article).permit(:title, :description))
-    @article.save
+    if @article.save
+      flash[:notice] = "Article was created successfully."
     redirect_to article_path(@article)
+    else
+      render "new", status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "Article was updated succesfully"
+      redirect_to @article
+    else
+      render 'edit', status: :unprocessable_entity
+    end
   end
 end
